76d7acc07d6418cd2e6b1c430a947d96e1e198e7
kern3l.pan1ck <kern3l.pan1ck.ksd@gmail.com>
2021-10-06 12:59:13 +0200

    Zmiana w drugim repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    private void displayText() {
+        System.out.println("Zmiana w drugim repo");
+    }
 }
